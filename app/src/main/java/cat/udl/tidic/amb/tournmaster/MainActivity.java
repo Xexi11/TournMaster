48f71dfc5959e2c9565cc44ac0a36859ca7591a7
Mane9mb <mmoreno9mb@gmail.com>
2020-03-15 20:41:00 +0100

    Clase User, Interface UserServices,
    fet la clase user + interficie del UserService + MainActivity per fer les instancies del les vistes.

--- app/src/main/java/cat/udl/tidic/amb/tournmaster/MainActivity.java
+++ app/src/main/java/cat/udl/tidic/amb/tournmaster/MainActivity.java
@@ -3,12 +3,60 @@ package cat.udl.tidic.amb.tournmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import cat.udl.tidic.amb.tournmaster.services.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    User us = new User();
+    EditText tokenEditText;
+    Button checkButton;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // elements del layout:
+        //----------------------
+        //tokenEditText = findViewById(R.id.editText_token);
+        //checkButton = findViewById(R.id.button_check);
+        //resultTextView = findViewById(R.id.textView_result);
+        UserService userService = RetrofitClientInstance.
+                getRetrofitInstance().create(UserService.class);
+
+        //Instancia per crear usuari en la base de dades
+        User user = new User();
+        user.setUsername("mane9mb");
+        user.setName("Manel");
+        user.setSurname("Moreno");
+        user.setPassword("170719");
+        user.setGenere("M");
+        user.setEmail("Manel_9mb@outlook.com");
+        Call<Void> call_post = userService.createUser(user);
+        call_post.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                System.out.println(response.code());
+                Log.d("MainActivity",String.valueOf(response.code()));
+
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println(t.getMessage().toString());
+                Log.d("MainActivity",t.getMessage());
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/cat/udl/tidic/amb/tournmaster/services/UserService.java
@@ -0,0 +1,36 @@
+package cat.udl.tidic.amb.tournmaster.services;
+
+import com.google.gson.JsonObject;
+
+import java.util.Map;
+
+import cat.udl.tidic.amb.tournmaster.User;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface UserService {
+
+    //metodes per fer les instancies a la base de dades tan como get i post!:
+
+        //@Headers("Authorization:656e50e154865a5dc469b80437ed2f963b8f58c8857b66c9bf")
+        @GET("/account/profile")
+        Call<User> getUserProfile(@HeaderMap Map<String, String> headers);
+
+        @Headers({"Content-type:application/json"})
+        @POST("/users/register")
+        Call<Void> createUser(@Body User user);
+
+        @Headers({"Content-type:application/json"})
+        @POST("/users/register")
+        Call<Void> createUser1(@Body JsonObject userJson);
+
+        @Headers({"Content-type:application/json"})
+        @POST("/users/register")
+        Call<Void> createUser2(@Body JsonObject userJson);
+
+
+}
