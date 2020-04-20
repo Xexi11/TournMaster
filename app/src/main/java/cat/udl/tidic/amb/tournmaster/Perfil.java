65c64310016c7f2a486597b5b16158726d42b204
Carlos Isaac Parra <56555192+strixZombis@users.noreply.github.com>
2020-04-20 19:52:45 +0200

    Perfil privado y publico

--- app/src/main/java/cat/udl/tidic/amb/tournmaster/Perfil.java
+++ app/src/main/java/cat/udl/tidic/amb/tournmaster/Perfil.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,9 +22,19 @@ import retrofit2.Response;
 
 public class Perfil extends AppCompatActivity {
     private TextView user;
-    private TextView mail;
-    private TextView rols;
-    private TextView sexo;
+    private EditText mail;
+    private EditText rol;
+    private EditText sex;
+    private EditText name;
+    private EditText surname;
+    private EditText birthday;
+    private EditText position;
+    private EditText prefsmash;
+    private EditText timeplay;
+    private EditText matchaname;
+    private EditText club;
+    private EditText license;
+    private Button iniciar;
 
 
     private UserService userService;
@@ -32,15 +44,24 @@ public class Perfil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil);
+        setContentView(R.layout.activity_profile);
 
 
 
         user = findViewById(R.id.text_users);
         mail = findViewById(R.id.text_mail);
-        rols = findViewById(R.id.rol);
-        sexo = findViewById(R.id.text_sexo);
-
+        rol = findViewById(R.id.text_rol);
+        sex = findViewById(R.id.text_sexo);
+        name = findViewById(R.id.text_name);
+        surname = findViewById(R.id.text_surname);
+        birthday = findViewById(R.id.text_birthday);
+        position = findViewById(R.id.text_position);
+        prefsmash = findViewById(R.id.text_prefsmash);
+        timeplay = findViewById(R.id.text_timeplay);
+        matchaname = findViewById(R.id.text_matchname);
+        club = findViewById(R.id.text_club);
+        license = findViewById(R.id.text_license);
+        iniciar = findViewById(R.id.inciarButton);
 
         userService = RetrofitClientInstance.
                 getRetrofitInstance().create(UserService.class);
@@ -68,10 +89,10 @@ public class Perfil extends AppCompatActivity {
 
                     if(user_sex.equals("M")){
                         Log.d("TAG","ENTRA");
-                        sexo.setText("Hombre");
+                        sex.setText("Hombre");
                     }
                     else{
-                        sexo.setText("Mujer");
+                        sex.setText("Mujer");
                     }
                     Log.d("TAG",user_sex);
                     String user_rol = userJson.get("rol").toString();
@@ -79,13 +100,48 @@ public class Perfil extends AppCompatActivity {
                     user_rol=user_rol.substring(1,user_rol.length()-1);
                     if(user_rol.equals("O")){
 
-                        rols.setText("Organizador");
+                        rol.setText("Organizador");
                     }
 
                     else{
-                        rols.setText("Jugador");
+                        rol.setText("Jugador");
                     }
-
+                    String name_user = userJson.get("name").toString();
+                    if(name_user.equals("")) {
+                        name.setText(atributs(nom_user));
+                    }
+                    String surname_user = userJson.get("surname").toString();
+                    if(surname_user.equals("")) {
+                        surname.setText(atributs(nom_user));
+                    }
+                    String birthday_user = userJson.get("birthdate").toString();
+                    if(birthday_user.equals("")) {
+                        birthday.setText(atributs(nom_user));
+                    }
+                    String position_user = userJson.get("position").toString();
+                    if(position_user.equals("")) {
+                        position.setText(atributs(nom_user));
+                    }
+                    String prefsmash_user = userJson.get("prefsmash").toString();
+                    if(prefsmash_user.equals("")) {
+                        prefsmash.setText(atributs(nom_user));
+                    }
+                    String timeplay_user = userJson.get("timeplay").toString();
+                    if(timeplay_user.equals("")) {
+                        timeplay.setText(atributs(nom_user));
+                    }
+                    String matchname_user = userJson.get("matchname").toString();
+                    if(matchname_user.equals("")) {
+                        matchaname.setText(atributs(nom_user));
+                    }
+                    String club_user = userJson.get("club").toString();
+                    if(club_user.equals("")) {
+                        club.setText(atributs(nom_user));
+                    }
+                    String license_user = userJson.get("license").toString();
+                    if(license_user.equals("")) {
+                        license.setText(atributs(nom_user));
+                    }
 
 
                 }
@@ -101,6 +157,7 @@ public class Perfil extends AppCompatActivity {
                 Log.d("TAG",t.getMessage().toString());
             }
         });
+
     }
     public String atributs(String n){
 
@@ -130,10 +187,6 @@ public class Perfil extends AppCompatActivity {
                         mPreferences.edit().putString("token", "").apply();
                         Intent intent = new Intent(Perfil.this, Login.class);
                         startActivity(intent);
-
-
-
-
                 }
 
 
@@ -164,5 +217,20 @@ public class Perfil extends AppCompatActivity {
         Intent intent = new Intent(Perfil.this,Perfil.class);
         startActivity(intent);
     }
+    public void editar(View view){
+        mail.setEnabled(true);
+        sex.setEnabled(true);
+        rol.setEnabled(true);
+        name.setEnabled(true);
+        surname.setEnabled(true);
+        birthday.setEnabled(true);
+        position.setEnabled(true);
+        prefsmash.setEnabled(true);
+        timeplay.setEnabled(true);
+        matchaname.setEnabled(true);
+        club.setEnabled(true);
+        license.setEnabled(true);
+    }
+
 
 }
